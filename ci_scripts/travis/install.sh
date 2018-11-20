56bea0556a0351dad00c1abe54faac8259c7fdce
Joel Nothman <joel.nothman@gmail.com>
2018-11-20 17:24:58 +1100

    Add environment.yml

--- ci_scripts/travis/install.sh
+++ ci_scripts/travis/install.sh
@@ -26,14 +26,12 @@ popd
 
 # Configure the conda environment and put it in the path using the
 # provided versions
-conda create -n testenv --yes python=$PYTHON_VERSION pip nose \
+conda env create -n testenv -f ../../environment.yml
+conda install --yes python=$PYTHON_VERSION pip nose \
       numpy=$NUMPY_VERSION scipy=$SCIPY_VERSION \
       scikit-learn=0.19
 
 source activate testenv
-
-conda install --yes  -c conda-forge -c flyem-forge graph-tool pygobject cairo
-conda install --yes -c pkgw/label/superseded gtk3
 
 if [[ "$COVERAGE" == "true" ]]; then
     pip install coverage coveralls
