f2fbefbfaa84713f5bf957d8aa7c958ce9787e7f
Joel Nothman <joel.nothman@gmail.com>
2018-12-17 17:29:46 +1100

    Establish the effect of random_state in tests

--- topsbm/tests/test_common.py
+++ topsbm/tests/test_common.py
@@ -1,17 +1,28 @@
 import numpy as np
+import pytest
 from sklearn.utils.estimator_checks import check_estimator
+from sklearn.datasets import fetch_20newsgroups, make_multilabel_classification
+from sklearn.feature_extraction.text import CountVectorizer
 from topsbm import TopSBM
+
+
+X_mmlc, _ = make_multilabel_classification(n_samples=20, n_features=100)
+bunch_20n = fetch_20newsgroups(categories=['alt.atheism',
+                                           'soc.religion.christian'],
+                               shuffle=True, random_state=0,
+                               remove=('headers', 'footers'))
+X_20n = CountVectorizer().fit_transform(bunch_20n.data)
 
 
 def test_transformer():
     return check_estimator(TopSBM)
 
 
-def test_random_state():
-    X = np.random.randint(0, 2, size=(10, 5))
+def test_random_state(n_samples=20, n_features=100):
+    feat = np.random.RandomState(0).choice(X_20n.shape[1], n_features)
+    X = X_20n[:n_samples, feat]
     Xt0a = TopSBM(random_state=0).fit_transform(X)
     Xt0b = TopSBM(random_state=0).fit_transform(X)
-    Xt1 = TopSBM(random_state=1).fit_transform(X)
-    assert np.allclose(Xt0a, Xt0b)
-    # XXX: confirm whether we should expect different results
-    # assert not np.allclose(Xt0a, Xt1)
+    Xt2 = TopSBM(random_state=2).fit_transform(X)
+    np.testing.assert_allclose(Xt0a, Xt0b)
+    assert Xt0a.shape != Xt2.shape or not np.allclose(Xt0a, Xt2)
