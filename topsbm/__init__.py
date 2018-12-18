68139bf56b055ac5110ba2cb1b6e461537e769b4
Joel Nothman <joel.nothman@gmail.com>
2018-12-18 16:54:57 +1100

    Centralise setting of version (#36)

--- topsbm/__init__.py
+++ topsbm/__init__.py
@@ -1,3 +1,15 @@
-from .transformer import TopSBM
 
-__all__ = ['TopSBM']
+try:
+    # This variable is injected in builtins by setup.py
+    # It is used to enable importing subpackages of sklearn when
+    # the binaries are not built
+    __IN_SETUP__
+except NameError:
+    __IN_SETUP__ = False
+
+if not __IN_SETUP__:
+    from .transformer import TopSBM
+    __all__ = ['TopSBM']
+
+
+__version__ = '0.1.dev1'
