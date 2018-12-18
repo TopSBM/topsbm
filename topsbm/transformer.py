476e31c7f3dd07fc00f1b1099dfcb76d7cfe640e
Joel Nothman <joel.nothman@gmail.com>
2018-12-18 11:25:24 +1100

    Fix licenses to gplv3

--- topsbm/transformer.py
+++ topsbm/transformer.py
@@ -1,6 +1,21 @@
 """
 This is the main file for implementing the hSBM for text mining.
 """
+# This file is part of TopSBM
+# Copyright 2017-8, Martin Gerlach and the University of Sydney
+#
+# TopSBM is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# TopSBM is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with TopSBM.  If not, see <https://www.gnu.org/licenses/>.
 
 from collections import defaultdict
 
