5518dba7a94c10cffde3d715801c2007e9e5f4da
Joel Nothman <joel.nothman@gmail.com>
2018-12-18 22:02:02 +1100

    Use pytest --rootdir

--- ci_scripts/travis/test.sh
+++ ci_scripts/travis/test.sh
@@ -2,15 +2,12 @@ set -e
 
 # Get into a temp directory to run test from the installed scikit learn and
 # check if we do not leave artifacts
+ROOT_DIR="$PWD"
 mkdir -p $TEST_DIR
 
 cd $TEST_DIR
 
-if [[ "$COVERAGE" == "true" ]]; then
-    nosetests -s --with-coverage --cover-package=$MODULE $MODULE
-else
-    nosetests -s $MODULE
-fi
+pytest --rootdir="$ROOT_DIR" --showlocals --pyargs topsbm
 
 pip install flake8
 flake8 topsbm examples
