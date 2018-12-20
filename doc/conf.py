203676ab17198ce39d9bc3b9a2c6ba23851b3f34
Joel Nothman <joel.nothman@gmail.com>
2018-12-20 14:11:27 +1100

    DOC use scipy sphinx theme

--- doc/conf.py
+++ doc/conf.py
@@ -15,8 +15,6 @@
 import sys
 import os
 
-import sphinx_rtd_theme
-
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
@@ -103,7 +101,7 @@ release = topsbm.__version__
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
-exclude_patterns = ['_build']
+exclude_patterns = ['_build', 'scipy-sphinx-theme']
 
 # The reST default role (used for this markup: `text`) to use for all
 # documents.
@@ -134,15 +132,20 @@ pygments_style = 'sphinx'
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'sphinx_rtd_theme'
+themedir = os.path.join(os.path.dirname(__file__), 'scipy-sphinx-theme', '_theme')
+if not os.path.isdir(themedir):
+    raise RuntimeError("Get the scipy-sphinx-theme first, "
+                       "via git submodule init && git submodule update")
+
+html_theme = 'scipy'
+# Add any paths that contain custom themes here, relative to this directory.
+html_theme_path = [themedir]
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
 #html_theme_options = {}
 
-# Add any paths that contain custom themes here, relative to this directory.
-html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
 # The name for this set of Sphinx documents.  If None, it defaults to
 # "<project> v<release> documentation".
